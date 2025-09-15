e1ede9db737d2c193759ed1dc0581017e8225b96
SheenaSuman <sheenasuman428@gmail.com>
2025-09-15 15:27:04 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Hero from './_section1/Hero';
+import Spotsection from './_section1/Spotsection';
+import Header from '../components/includes/Header';
+import Footer from '../components/includes/Footer';
+import Promo from '../app/_section1/Promo';
+const Page = () => {``
+  return (
+  <div className="bg-[#1C1C1C] text-white">
+  <Promo />
+  <Header />
+  <Hero />
+  <Spotsection/>
+  <Footer/> 
+
+</div>
+);
+};
+
+
+export default Page;  
